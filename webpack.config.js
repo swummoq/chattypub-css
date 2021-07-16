bd778c82d4b7348d967c331406f2c4c2cf7de7d8
deckdeckgo <hello@deckdeckgo.com>
2021-07-16 09:57:03 +0000

    chore: update project settings

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ const config = {
     devServer: {
         open: true,
         port: 3000,
-        publicPath: '{{DECKDECKGO_BASE_HREF}}',
-        openPage: 'http://localhost:3000{{DECKDECKGO_BASE_HREF}}'
+        publicPath: '/heerko/chattypub-css/',
+        openPage: 'http://localhost:3000/heerko/chattypub-css/'
     }
 };
 
